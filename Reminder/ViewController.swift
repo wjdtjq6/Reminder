a639448c9d460193c83cae1a6ea36e6e2cd0a916
jimmy <wjdtjq6@gmail.com>
2024-07-02 16:56:19 +0900

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by t2023-m0032 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
